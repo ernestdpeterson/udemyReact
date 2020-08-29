0c1c8125f37a8203f6fab37530995af8d2357bf9
Ernest D. Peterson <petesplace312@yahoo.com>
2020-08-29 12:12:04 -0500

    Updates

---
+++ precache-manifest.0ecfeaf7ff127c38af5da19dc2426f8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5d3c76af206c1f2d4736338aa55407",
+    "url": "/udemyReact/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/udemyReact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "68ecbd99effb45fffbf5",
+    "url": "/udemyReact/static/css/main.36e0d46a.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/udemyReact/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemyReact/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ecbd99effb45fffbf5",
+    "url": "/udemyReact/static/js/main.4d9949fe.chunk.js"
+  },
+  {
+    "revision": "0dd1ad9f8abd8a15ba15",
+    "url": "/udemyReact/static/js/runtime-main.768358b0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/udemyReact/static/media/SEGA.12a3db8b.woff"
+  }
+]);
